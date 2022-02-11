b6cbe16a295478cdd1478dba8fe7a56045c99a07
ainkham <99156525+ainkham@users.noreply.github.com>
2022-02-11 20:40:11 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    protected String name ;
+	protected int age;
+	public Person(int Age, String aName) { 
+	age = Age ; name = aName; 
+	} 
+	 private int getAge(){
+		return age; 
+	}
+		public void printlnformation(){
+			System.out.println("Age"+getAge());
+	}}
+	public class Instructor extends Person{
+		public void assign(){
+	}}
+	class Student extends Person{
+		protected int year;
+	}
+	class Select {
+		protected float mark;
+		protected String location;
+	}
+	class Course {
+		protected String name;
+		protected double grade;
+	}
+		
